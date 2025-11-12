6e1eb8efc05dfbb11fdbcf14936e72f65f5178c2
Eduardo Vinicius Mompiam de Lima <149838078+EduardooVi@users.noreply.github.com>
2025-11-12 19:55:53 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConversaoImplicitaeCasting
+{ 
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            float b;
+            int c;
+            int d = 5;
+            int e = 2;
+            double resultado = (double)d / e;
+
+            Console.WriteLine(resultado);
+            Console.WriteLine("\n\n\n\n");
+
+            a = 5.1;
+            b = (float)a;
+            c = (int)a;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
